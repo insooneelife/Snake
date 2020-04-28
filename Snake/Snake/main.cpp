80fcb4a1324ce80bb279bcc433ed3598b72904ce
unknown <insooneelife@naver.com>
2020-04-28 10:34:43 +0900

    temp

--- Snake/Snake/main.cpp
+++ Snake/Snake/main.cpp
@@ -64,39 +64,39 @@ bool loop(int& inputDir)
 int main(int argc, char *argv[])
 {
 	// sdl
+	
+	AllocConsole();
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+	// Enable standard application logging 
+	SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		AllocConsole();
-
-		SDL_Window *window;
-
-		// Enable standard application logging 
-		SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
-
-		// Initialize SDL
-		if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		{
-			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
-			return (1);
-		}
-
-		// Set 640x480 video mode
-		window = SDL_CreateWindow(
-			"CheckKeys Test",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			640, 480, 0);
-		if (!window)
-		{
-			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create 640x480 window: %s\n",
-				SDL_GetError());
-			Utility::quit(2);
-		}
-
-		SDL_StartTextInput();
-
-		// Print initial modifier state 
-		SDL_PumpEvents();
-		Utility::PrintModifierState();
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
+		return (1);
 	}
+
+	// Set 640x480 video mode
+	bool success = SDL_CreateWindowAndRenderer(
+		640, 480, 0, &window, &renderer);
+
+	if (!window)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create 640x480 window: %s\n",
+			SDL_GetError());
+		Utility::quit(2);
+	}
+
+	SDL_StartTextInput();
+
+	// Print initial modifier state 
+	SDL_PumpEvents();
+	Utility::PrintModifierState();
+	
 	
 
 
@@ -106,12 +106,27 @@ int main(int argc, char *argv[])
 	world.init();
 	int stepCnt = 0;
 
+	static SDL_Point points[4] = {
+		{ 320, 200 },
+		{ 300, 240 },
+		{ 340, 240 },
+		{ 320, 200 }
+	};
+
 	while (true) 
 	{
 		// input
 		int inputDir = -1;
 		loop(inputDir);
 			
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderDrawLines(renderer, points, 4);
+		SDL_RenderPresent(renderer);
+
 		if (!world.update(inputDir))
 		{
 			break;
