37cf959cbcdcf5954068656b8ca685cb3acefa71
unknown <insooneelife@naver.com>
2020-04-28 11:32:22 +0900

    sfml render

--- Snake/Snake/main.cpp
+++ Snake/Snake/main.cpp
@@ -1,10 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <windows.h>
+#include <ctime>
+#include "World.h"
 #include <SFML/Graphics.hpp>
+
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(640, 640), "SFML works!");
+	
+
+	srand((unsigned int)time(0));
+	World world;
+
+	world.init();
+	int stepCnt = 0;
 
 	while (window.isOpen())
 	{
@@ -15,9 +29,38 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
+
+		int inputDir = -1;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			inputDir = Left;
+		}
+		else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			inputDir = Right;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			inputDir = Up;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			inputDir = Down;
+		}
+
+		if (!world.update(inputDir))
+		{
+			break;
+		}
+
+
+		window.clear(sf::Color::White);
+
+		world.render(window);
+
 		window.display();
+
+		Sleep(50);
 	}
 
 	return 0;
